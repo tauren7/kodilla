34e428ef926af23f907ba8065251c07d541b3f8d
Mariusz Szpala <mariusz.szpala>
2017-04-01 15:55:38 +0200

    Grunt1

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'main.css': 'main.sass'
+                }
+            }
+        },
+
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'images/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'images/build/'
+                }]
+            }
+        },
+
+        watch: {
+            scripts: {
+                files: ['sass/*.sass'],
+                tasks: ['css'],
+                options: {
+                    spawn: false,
+                },
+            }
+        }
+
+    });
+    // Load the plugins tasks 
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+    // Default task(s).
+
+    grunt.registerTask('default', ['sass', 'imagemin', 'watch']);
+};
